8f45c60040941b8064e9f992a92e530d9ce62a9a
Luiza Valezim <62949391+LuizaValezim@users.noreply.github.com>
2021-06-11 14:32:55 -0300

    Adicionando randomizador de temas

---
+++ randomiza_tema.py
@@ -0,0 +1,27 @@
+import random
+
+temas = [
+    'criatividade',
+    'livros',
+    'séries de tv',
+    'american way of life',
+    'anos 60',
+    'anos 70',
+    'anos 80',
+    'anos 2000',
+    'sociedade retro-futurista',
+    'internet',
+    'mafia italiana',
+    'era medieval',
+    'renascimento',
+    'cultura nórdica',
+    'egito',
+    'romanos',
+    ''
+    'solarpunk',
+    'agatha christie',
+    '1984',
+]
+
+
+print('o tema escolhido para a nossa conversa de hoje é: {0}'.format(random.choice(temas)))
